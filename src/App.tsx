fb7372bf723b324f94820d0609cc5f7b210b2483
ssani7 <sanaullah.sani756@gamil.comm>
2024-10-30 13:01:00 +0600

    only first name

--- src/App.tsx
+++ src/App.tsx
@@ -182,14 +182,14 @@ const App: React.FC = () => {
 						<div className="flex items-center w-2/3 border-2 border-[#43433b] rounded-full px-4 py-[2px] bg-[#43433b]/[0.6] max-w-64">
 							<img src={binanceLogo} alt="Exchange" className="w-8 h-8" />
 							<div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
-							<div className="flex-1 text-center">
+							{/* <div className="flex-1 text-center">
 								<p className="text-xs text-[#85827d] font-medium">Profit per hour</p>
 								<div className="flex items-center justify-center space-x-1">
 									<img src={dollarCoin} alt="Dollar Coin" className="w-[18px] h-[18px]" />
 									<p className="text-sm">{formatProfitPerHour(profitPerHour)}</p>
 									<Info size={20} className="text-[#43433b]" />
 								</div>
-							</div>
+							</div> */}
 							<div className="h-[32px] w-[2px] bg-[#43433b] mx-2"></div>
 							<Settings className="text-white" />
 						</div>
@@ -239,10 +239,10 @@ const App: React.FC = () => {
 
 			{/* Bottom fixed div */}
 			<div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
-				<div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
+				{/* <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl">
 					<img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
 					<p className="mt-1">Exchange</p>
-				</div>
+				</div> */}
 				<div className="text-center text-[#85827d] w-1/5">
 					<Mine className="w-8 h-8 mx-auto" />
 					<p className="mt-1">Mine</p>
